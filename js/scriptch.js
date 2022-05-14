aaa3c53b74e3a064a5aa09948da079a177494da0
nicofiori11 <rauldk_fiori@live.com>
2022-05-14 18:45:03 -0300

    Cargando proyecto finalizado a master

---
+++ js/scriptch.js
@@ -0,0 +1,69 @@
+
+var textoentrada = document.querySelector("#inicial");
+var textosalida = document.querySelector(".textoresultado");
+var Bencriptar = document.querySelector(".botonEncriptar");
+var Bdesencriptar = document.querySelector(".botonDesencriptar");
+var cuadroresultado = document.querySelector(".notexto")
+var cuadrocopiar = document.querySelector(".ocultar")
+var Bcopiar = document.querySelector(".botonCopiar")
+function encriptar(){
+    var texto = textoentrada.value;
+    texto = texto.toLowerCase()
+    var resultado = texto.replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o","ober")
+    .replaceAll("u","ufat");
+    textosalida.value = resultado
+}
+function desencriptar(){
+    var texto = textoentrada.value;
+    texto = texto.toLowerCase()
+    var resultado = texto.replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober","o")
+    .replaceAll("ufat","u");
+    textosalida.value = resultado
+}
+
+Bencriptar.addEventListener ( "click",function (){
+    if(textoentrada.value != ""){
+        cuadroresultado.classList.remove("notexto");
+        cuadroresultado.classList.add("ocultar");
+        cuadrocopiar.classList.remove("ocultar");
+        cuadrocopiar.classList.add("sitexto");
+    }
+    else{
+        cuadroresultado.classList.remove("ocultar");
+        cuadroresultado.classList.add("notexto");
+        cuadrocopiar.classList.remove("sitexto");
+        cuadrocopiar.classList.add("ocultar");
+    }
+
+});
+Bdesencriptar.addEventListener ( "click",function (){
+    if(textoentrada.value != ""){
+        cuadroresultado.classList.remove("notexto");
+        cuadroresultado.classList.add("ocultar");
+        cuadrocopiar.classList.remove("ocultar");
+        cuadrocopiar.classList.add("sitexto");
+    }
+    else{
+        cuadroresultado.classList.remove("ocultar")
+        cuadroresultado.classList.add("notexto");
+        cuadrocopiar.classList.remove("sitexto");
+        cuadrocopiar.classList.add("ocultar");
+    }
+
+});
+Bcopiar.addEventListener("click", function(){
+    textosalida.focus();
+    navigator.clipboard.writeText(textosalida.value)
+})
+
+Bdesencriptar.onclick = desencriptar;
+Bencriptar.onclick = encriptar;
+
+
+
